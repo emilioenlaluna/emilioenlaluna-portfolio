3466e19d90251b8c3b9ab8083f28492c3e3c9fea
Emilio Luna <al244182@edu.uaa.mx>
2024-01-24 20:26:42 -0600

    Empty components and PWA config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { EducationComponent } from './components/education/education.component';
+import { CertificationsComponent } from './components/certifications/certifications.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { VolunteeringComponent } from './components/volunteering/volunteering.component';
+import { AwardsComponent } from './components/awards/awards.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { MeComponent } from './components/me/me.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResumeComponent,
+    EducationComponent,
+    CertificationsComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    VolunteeringComponent,
+    AwardsComponent,
+    ExperienceComponent,
+    MeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
