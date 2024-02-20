2d7c8e47bc0baa80e9b98410226698320840af3e
Emilio Luna <al244182@edu.uaa.mx>
2024-02-20 11:58:21 -0600

    Images and navbar

--- src/assets/marvel/js/custom.js
+++ src/assets/marvel/js/custom.js
@@ -2,13 +2,16 @@
 
   "use strict";
 
-  
+    // COLOR MODE
+    $('.color-mode').click(function(){
+        $('.color-mode-icon').toggleClass('active')
+        $('body').toggleClass('dark-mode')
+    })
 
     // HEADER
     $(".navbar").headroom();
 
-    // PROJECT CAROUSEL
-    
+ 
 
     // SMOOTHSCROLL
     $(function() {
--- src/assets/portfolio/js/main.js
+++ src/assets/portfolio/js/main.js
@@ -18,39 +18,11 @@
   }
   burgerMenu();
 
-  var siteIstotope = function() {
-	  var $container = $('#portfolio-grid').isotope({
-	    itemSelector : '.item',
-	    isFitWidth: true
-	  });
-
-	  $(window).resize(function(){
-	    $container.isotope({
-	      columnWidth: '.col-sm-3'
-	    });
-	  });
-	  
-	  $container.isotope({ filter: '*' });
-
-	  $('#filters').on( 'click', 'a', function(e) {
-	  	e.preventDefault();
-	    var filterValue = $(this).attr('data-filter');
-	    $container.isotope({ filter: filterValue });
-	    $('#filters a').removeClass('active');
-	    $(this).addClass('active');
-	  });
-  }
-  $(window).on('load', function () {
-    siteIstotope();
-  });
+ 
 
 
 
 
 })(jQuery);
 
-AOS.init({
-	easing: 'ease',
-	duration: 1000,
-	once: true
-});
+
